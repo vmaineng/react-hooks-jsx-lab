c275a69ad0ab337eb64752ef2b294dd261666ad4
Mai Vang <vmaineng@gmail.com>
2022-05-16 16:01:24 -0700

    commit tree 215a90d08619f94eaa2f097c39dbc269e75dc27c

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,9 +1,15 @@
 import React from "react";
 import { name, city } from "../data/data.js";
 
+const divStyle = {
+  color: 'firebrick'
+};
+
 function Home() {
   // update the JSX being returned!
-  return <div>Home</div>;
+  return <div id="home" style={divStyle}>
+    <h1>"Your {name} is a Web Developer from Your {city}"</h1>
+  </div>;
 }
 
 export default Home;
